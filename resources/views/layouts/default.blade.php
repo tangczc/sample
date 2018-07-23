f05b81bbf5902478df5eefc9a6f347fec9ab9877
rootchen <Mytczc521@outlook.com>
2018-07-23 14:31:14 +0000

    link Css

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,22 +5,12 @@
     <link rel = "stylesheet" href="/css/app.css"/>
 </head>
 <body>
-    <header class = "navbar navbar-fixed-top navbar-inverse">
-        <div class = "container">
-            <div class = "col-md-offset-0 col-md-12">
-                <a href = "/" id = "logo">Sample App</a>
-                <nav>
-                    <ul class = "nav navbar-nav navbar-right">
-                        <li><a href = "/help">帮助</a></li>
-                        <li><a href = "#">登陆</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </header>
-    <div class ="container">
+    @include('layouts._header')
+    <div class="container">
+      <div>
         @yield('content')
+        @include('layouts._footer')
+      </div>
     </div>
-    
 </body>
 </html>
