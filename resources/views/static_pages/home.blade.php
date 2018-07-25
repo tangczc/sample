6e29ee3ad082f679ee45dd79de8585de8cf3d15c
rootchen <Mytczc521@outlook.com>
2018-07-25 15:10:36 +0000

    change home route

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -5,7 +5,7 @@
         <h1>欢迎进入唐朝早晨的小黑屋</h1>
         <p>点击下方注册</p>
         <p class = "lead">
-            <a class = "btn btn-lg btn-success" href = "{{ route('signup')}}">现在注册</a>
+            <a class = "btn btn-lg btn-success" href = "{{ route('users.signup')}}">现在注册</a>
         </p>
     </div>
 @stop
