ae72f2f036a244cdc607a2adda5e73f09667b591
rootchen <Mytczc521@outlook.com>
2018-08-01 14:28:38 +0000

    Add user following

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {    Model::unguard();
          $this->call(UsersTableSeeder::class);
          $this->call(StatusesTableSeeder::class);
+         $this->call(FollowersTableSeeder::class);
          Model::reguard();
 
     }
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
-@section('title','主页')
+
 @section('content')
-@if (Auth::check())
+  @if (Auth::check())
     <div class="row">
       <div class="col-md-8">
         <section class="status_form">
@@ -14,15 +14,23 @@
         <section class="user_info">
           @include('shared._user_info', ['user' => Auth::user()])
         </section>
+        <section class="stats">
+          @include('shared._stats', ['user' => Auth::user()])
+        </section>
       </aside>
     </div>
   @else
-    <div class = "jumbotron">
-        <h1>欢迎进入唐朝早晨的小黑屋</h1>
-        <p>点击下方注册</p>
-        <p class = "lead">
-            <a class = "btn btn-lg btn-success" href = "{{ route('users.signup')}}">现在注册</a>
-        </p>
-    </div>
-    @endif
+    <div class="jumbotron">
+      <h1>Hello Laravel</h1>
+      <p class="lead">
+        你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">Laravel 入门教程</a> 的项目主页。
+      </p>
+      <p>
+        一切，将从这里开始。
+      </p>
+      <p>
+        <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+      </p>
+    </div>    
+  @endif
 @stop
