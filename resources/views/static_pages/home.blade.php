9b2842a12de3bfc73dd595f8f6fc383994c1b6fb
rootchen <Mytczc521@outlook.com>
2018-08-07 12:34:27 +0000

    change homepage

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -23,7 +23,7 @@
     <div class="jumbotron">
       <h1>Hello Laravel</h1>
       <p class="lead">
-        你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">Laravel 入门教程</a> 的项目主页。
+       欢迎加入唐朝早晨的小黑屋~~
       </p>
       <p>
         一切，将从这里开始。
